e5252a2124b137806c9a983a03779b49cdede2ca
uzzal71 <uzzalroy.acm@gmail.com>
2020-04-07 01:15:00 +0600

    Adding templates to your application

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hi!"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True) # also define a port app.run(debug=True, port=50001)
