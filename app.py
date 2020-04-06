28587f6b97db46554ecc8a0b8faf57e089d096d7
uzzal71 <uzzalroy.acm@gmail.com>
2020-04-07 00:51:59 +0600

    Understanding app.py file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return "Hi!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) # also define a port app.run(debug=True, port=50001)
