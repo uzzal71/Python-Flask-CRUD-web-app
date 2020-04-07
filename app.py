8b5cf5cd83df17306cd05415e7cd7be5411c532f
uzzal71 <uzzalroy.acm@gmail.com>
2020-04-07 14:31:55 +0600

    Adding Bootstrap CSS to your application

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -10,6 +12,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/css')
+def css():
+    return render_template('css.html')
 
 if __name__ == '__main__':
     app.run(debug=True) # also define a port app.run(debug=True, port=50001)
